b7e2e7729357206b4b0af522045ec8d332f0239d
Sahil Gandhi <sahilgandhi@Sahils-MacBook-Pro.local>
2018-07-16 23:41:00 -0400

    Apollo client initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { AppComponent } from './app.component';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ListComponent } from './list/list.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ListComponent],
+  imports: [BrowserModule, HttpClientModule, ApolloModule, HttpLinkModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({ uri: 'https://vm8mjvrnv3.lp.gql.zone/graphql' }),
+      cache: new InMemoryCache()
+    });
+  }
+}
